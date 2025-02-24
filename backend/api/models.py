8469b023031bc47c8476d7f31630a4db8321960f
Студент ТюмГУ <student@study.utmn.ru>
2025-02-24 20:25:36 +0500

    register

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+# from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# # Create your models here.
+# class User(AbstractUser):
+#     email = models.EmailField(unique=True)
+#     password = models.CharField(max_length=255)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     username = None
+
+#     USERNAME_FIELD = "email"
+#     REQUIRED_FIELDS = []
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,24 @@
+#from .models import User
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "email", "pas   sword"]
+        fields = ["id", "email", "password", 'date_joined']
+        extra_kwargs = {
+            'date_joined': {'read_only': True},
+            'password': {'write_only': True}
+        }
+        
+    # def create(self, validated_data):
+    #     print(validated_data)
+    #     user = User.objects.create_user(**validated_data)
+    #     return user
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
